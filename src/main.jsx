648f83e766682634610892b07e6febb2395b71e6
Maryoomah <148255944+Maryoomah@users.noreply.github.com>
2026-01-31 01:30:53 +0100

    Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <HashRouter>
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <Navbar />
+
+      <App />
+    </StrictMode>
   </HashRouter>,
-)
+);
